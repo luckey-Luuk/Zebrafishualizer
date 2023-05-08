8a02ce30aeb5295815d912a195d3c68972f7c74e
RGijsbers <r.g.gijsbers@gmail.com>
2023-05-08 14:01:00 +0200

    This version shows the pointcloud on 1 layer from here i want to try and save the pointcould

--- PointCloud/mayavi.py
+++ PointCloud/mayavi.py
@@ -1,15 +1,16 @@
-import tifffile
 import numpy as np
-import matplotlib.pyplot as plt
-# import pywavefront
+import tifffile
 from pywavefront import Wavefront
+import matplotlib.pyplot as plt
+from skimage import io, color
+from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d import Axes3D
-from PIL import Image
+from pyntcloud import PyntCloud
+import pandas as pd
 
-
-data = tifffile.imread('/Users/robertgijsbers/Desktop/20190701--2/20190701--20119.tif') #path name to tiff file goes here
+data = tifffile.imread('Data/20190701--2/20190701--20119.tif') #path name to tiff file goes here
 # volume = np.array(data)
-im = Image.open('/Users/robertgijsbers/Desktop/20190701--2/20190701--20119.tif')
+# im = Image.open('Data/20190701--2/20190701--20119.tif)
 
 num_layers, height, width = 8, 512, 512 #initialize number of layers, height and width
 
@@ -32,25 +33,19 @@ z_coords = np.zeros(combined_point_cloud.shape[0])
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(combined_point_cloud[:,0], combined_point_cloud[:,1], z_coords, s=1, cmap='viridis')
-# plt.show()
+plt.show()
 
-output_file = "output.obj"
-with open(output_file, 'w') as f:
-    pass
+#till here it works only the points are at a single z-axis.
 
+# output_file = "output.obj"
+# with open(output_file, 'w') as f:
+#     pass
 
+# obj = Wavefront(output_file)
 
-obj = Wavefront(output_file)
-
-# Add the point cloud vertices to the object
-for layer in range(8): # assuming 8 layers in the Z direction
-    for point in range(len(point_clouds[layer])):
-        rows, cols = np.where(point_clouds[layer][point] == 1)
-        z = layer
-        if rows.size > 0 and cols.size > 0:
-            for x, y in zip(rows, cols):
-                obj.vertices.append((x, y, z))
-
-# Save the object to a file
-with open(output_file, "w") as f:
-    f.write(str(obj))
+# output_file = "point_cloud.obj"
+# output_file = "point_cloud.obj"
+# with open(output_file, "w") as f:
+#     for point in combined_point_cloud:
+#         f.write(f"v {point[0]} {point[1]} {point[2]}\n")
+#     f.write(f"f {' '.join(str(i + 1) for i in range(1, combined_point_cloud.shape[0] + 1))}\n")

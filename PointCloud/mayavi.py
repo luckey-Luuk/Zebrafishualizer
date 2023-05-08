ad581f05a179c260a10ed4fcdd6e0b22de4bb9e5
RGijsbers <r.g.gijsbers@gmail.com>
2023-05-08 15:43:50 +0200

    It works on multiple layers now the layers should be connected to eachother

--- PointCloud/mayavi.py
+++ PointCloud/mayavi.py
@@ -1,22 +1,12 @@
 import numpy as np
 import tifffile
-# from pywavefront import Wavefront
 import matplotlib.pyplot as plt
-# from skimage import io, color
-# from sklearn.cluster import KMeans
-# from mpl_toolkits.mplot3d import Axes3D
-# from pyntcloud import PyntCloud
-# import pandas as pd
 import trimesh
 
 data = tifffile.imread('Data/20190701--2/20190701--20119.tif') #path name to tiff file goes here
-# volume = np.array(data)
-# im = Image.open('Data/20190701--2/20190701--20119.tif)
-
-num_layers, height, width = 8, 512, 512 #initialize number of layers, height and width
 
 imarray = np.array(data)
-# print(imarray.shape)
+num_layers, height, width = imarray.shape[0], imarray.shape[1], imarray.shape[2] # initialize number of layers, height and width
 grayscale_threshold = 200
 
 
@@ -39,16 +29,14 @@ for z in range(num_layers):
 combined_point_cloud = np.concatenate(point_clouds)
 colors = np.concatenate(colors)
 
-# print(combined_point_cloud)
-z_coords = np.zeros(combined_point_cloud.shape[0])
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(combined_point_cloud[:, 0], combined_point_cloud[:, 1], z_coords, s=1, c=colors / 255.0)
+ax.scatter(combined_point_cloud[:, 0], combined_point_cloud[:, 1], combined_point_cloud[:, 2],
+           s=1, c=colors / 255.0)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 plt.show()
-
-#till here it works only the points are at a single z-axis.
-
 
 # Create a trimesh object
 mesh = trimesh.Trimesh(vertices=combined_point_cloud)

c532b143cb5f98072787baa7bd93c44efbeac772
Luuk Deen <LuukDeen@hotmail.com>
2023-06-30 09:59:38 +0200

    add meshes

--- Mesh/mesh.py
+++ Mesh/mesh.py
@@ -10,7 +10,7 @@ import vtk
 
 def save_mesh(mesh, foldername, filename, overwrite=False):
     if not os.path.exists(foldername): # create folder if it doesn't exist
-        os.mkdir(foldername)
+        os.makedirs(foldername)
     elif os.path.exists(foldername + '/' + filename + '.stl'): # check if file needs to be overwritten
         if overwrite:
             os.remove(foldername + '/' + filename + '.stl')
@@ -19,7 +19,7 @@ def save_mesh(mesh, foldername, filename, overwrite=False):
     mesh.save(foldername + '/' + filename + '.stl') # save mesh
 
 source = "Data/convertedToImagej/20190701--2_inter_29layers_mask_imagej" # folder containing labeled tif files
-target_folder = "Data/meshes/20190701--2_meshes" # folder to save mesh files
+target_folder = "Data/meshes/20190701--2" # folder to save mesh files
 
 
 for filename in os.listdir(source):
@@ -29,20 +29,23 @@ for filename in os.listdir(source):
     f = os.path.join(source, filename) # get file path
     if not f.endswith(".tif"): # check if file is a tif file
         print("Not a tif file: " + f)
+        continue
         #TODO: check if tifs have the same shape
     else:
         imarray = np.array(tifffile.imread(f)) # read tif file
         num_layers, height, width = imarray.shape[0], imarray.shape[1], imarray.shape[2] # initialize number of layers, height and width
-        num_cells = imarray.max() # get number of cells in tif
+        min_cell = sorted(set(imarray.flatten()))[1] # get lowest cell label (exclude empty space)
+        max_cell = imarray.max() # get highest cell label
 
 
        # Create point cloud
-        point_clouds = [ [] for _ in range(num_cells) ]
+        # point_clouds = [ [] for _ in range(num_cells) ]
+        point_clouds = [ [] for _ in range(max_cell+1) ]
         for z in range(num_layers):
             layer = imarray[z, :, :] # select layer
 
-            for c in range(0, num_cells):
-                matching_pixels = np.where(layer == c+1) # find pixels with current cell label
+            for c in range(min_cell, max_cell+1):
+                matching_pixels = np.where(layer == c) # find pixels with current cell label
                 point_cloud = np.column_stack(matching_pixels).astype(float) # add pixels to point cloud and convert to float to handle non-integer coordinates
                 point_cloud[:, 0] -= height / 2
                 point_cloud[:, 1] -= width / 2
@@ -50,49 +53,53 @@ for filename in os.listdir(source):
                 
                 point_clouds[c].append(point_cloud)
 
-        clouds = [None] * num_cells
-        for c in range(0, num_cells):
-            clouds[c] = pv.PolyData(np.concatenate(point_clouds[c])) # combine layers into one and convert to polydata
+        clouds = [None] * (max_cell+1)
+        for c in range(min_cell, max_cell+1):
+            if point_clouds[c]:
+                clouds[c] = pv.PolyData(np.concatenate(point_clouds[c])) # combine layers into one and convert to polydata
 
 
        # Create mesh
         frame = pv.PolyData() # initialize mesh with all cells
 
        # create mesh of each cell
-        for c in range(0, num_cells):
-            # cell = clouds[c].delaunay_3d(alpha=3, tol=0.1, offset=2.5).extract_geometry() # use delaunay to create mesh, alpha variable dictates how close the points have to be to get connected
-            cell = clouds[c].reconstruct_surface(sample_spacing=1) # use surface reconstruction to create mesh
-            # cell = cell.delaunay_3d(alpha=3, tol=0.1, offset=2.5).extract_geometry() # use delaunay on mesh
+        for c in range(min_cell, max_cell+1):
+            if clouds[c]:
+                # cell = clouds[c].delaunay_3d(alpha=3, tol=0.1, offset=2.5).extract_geometry() # use delaunay to create mesh, alpha variable dictates how close the points have to be to get connected
+                cell = clouds[c].reconstruct_surface(sample_spacing=1) # use surface reconstruction to create mesh
+                # cell = cell.delaunay_3d(alpha=3, tol=0.1, offset=2.5).extract_geometry() # use delaunay on mesh
 
-           # Fix mesh
-            meshfix = mf.MeshFix(cell)
-            meshfix.repair()
-            repaired = meshfix.mesh
+               # Fix mesh
+                meshfix = mf.MeshFix(cell)
+                meshfix.repair()
+                repaired = meshfix.mesh
 
-           # Perform mesh smoothing
-            smoother = vtk.vtkSmoothPolyDataFilter()
-            smoother.SetInputData(repaired)
-            smoother.SetNumberOfIterations(500)
-            smoother.Update()
-            smoothed_cell = smoother.GetOutput() # get the smoothed mesh
-            smoothed_cell_pv = pv.wrap(smoothed_cell) # convert the VTK data to a PyVista mesh
+               # Perform mesh smoothing
+                smoother = vtk.vtkSmoothPolyDataFilter()
+                smoother.SetInputData(repaired)
+                smoother.SetNumberOfIterations(500)
+                smoother.Update()
+                smoothed_cell = smoother.GetOutput() # get the smoothed mesh
+                smoothed_cell_pv = pv.wrap(smoothed_cell) # convert the VTK data to a PyVista mesh
 
-            # plotter = pv.Plotter() # create PyVista plotter
-            # plotter.add_mesh(smoothed_cell_pv) # add smoothed mesh to plotter
-            # plotter.show()
+                # plotter = pv.Plotter() # create PyVista plotter
+                # plotter.add_mesh(smoothed_cell_pv) # add smoothed mesh to plotter
+                # plotter.show()
 
-            # smooth = repaired.smooth(n_iter=5000)
-            # smooth.plot()
-            # smooth_taubin = repaired.smooth_taubin(n_iter=5000, pass_band=0.5)
-            # smooth_taubin.plot()
+                # smooth = repaired.smooth(n_iter=5000)
+                # smooth.plot()
+                # smooth_taubin = repaired.smooth_taubin(n_iter=5000, pass_band=0.5)
+                # smooth_taubin.plot()
 
-           # Save cell mesh
-            save_mesh(smoothed_cell_pv, target_folder + '/' + shortened_filename, shortened_filename + '-' + str(c+1))
+               # Save cell mesh
+                save_mesh(smoothed_cell_pv, target_folder + '/' + shortened_filename, shortened_filename + '-' + str(c))
 
-            frame = frame.merge(smoothed_cell_pv) # add cell mesh to frame mesh
+                frame = frame.merge(smoothed_cell_pv) # add cell mesh to frame mesh
         
         # frame.plot()
         save_mesh(frame, target_folder, shortened_filename)
+
+        break # uncomment to create only meshes of first tif file
 
 
     #     conn = smoothed_cell_pv.connectivity(largest=False) # get connectivity of mesh
@@ -118,5 +125,3 @@ for filename in os.listdir(source):
     #     #             os.mkdir(target_folder + '/bodies/' + os.path.splitext(filename)[0].split('_')[0])
     #     #         body.save(target_folder + '/bodies/' + os.path.splitext(filename)[0].split('_')[0] + '/body'+str(bodiesPolyData.index(body))+'.stl')
     #     # bodies.plot()
-    
-        break # uncomment to create only meshes of first tif file
